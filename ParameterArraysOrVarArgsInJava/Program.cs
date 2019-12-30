10e5e7f7e3a8288346162ecb6268c3429c7d14c3
Ediezzle <edmore.mandikiyana@gmail.com>
2019-12-30 17:20:03 +0200

    final commit

---
+++ ParameterArraysOrVarArgsInJava/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ParameterArraysOrVarArgsInJava
+{
+    class Program
+    {
+        static void Foo(int x, params string[] values)
+        {
+            Console.Write(x);
+            for (int i = 0; i < values.Length - 1; i++)
+            {
+                Console.Write(", " + values[i]);  
+            }
+            
+        }
+        static void Main(string[] args)
+        {
+            Foo(5, "Hazy", "Dee");
+
+            Console.WriteLine();
+
+            Foo(12, "Eddaz", "Irv", "Laatie");
+            string[] array = { "Ellaz", "Henso", "OurOwnDavido", "Rue"};
+
+            Console.WriteLine();
+
+            Foo(8, array);
+        }
+    }
+}
